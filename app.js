6c92f786cabe28d2f63b4ba9120da79bdbd4cd30
Schomburg <schomburg.michael90@gmail.com>
2018-07-25 20:35:13 +0200

    done before cleanup

--- app.js
+++ app.js
@@ -5,31 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const startButton = document.querySelector('.btn__reset');
   const lifes = document.getElementById('scoreboard');
   const tries = document.getElementsByClassName('tries');
+  const overlay = document.getElementById('overlay');
+  const title = document.querySelector('.title');
   let missed = 0;
 
-  var phrases = ["January",
-    "Februrary",
-    "April",
-    "November",
-    "August",
-    "October",
-    "December"
+  var phrases = [
+    "January is a nice month",
+    "Februrary is a nice month",
+    "April is a nice month",
+    "November is a nice month",
+    "August is a nice month",
+    "October is a nice month",
+    "December is a nice month"
+
   ];
 
-  // Eventlistener which fades and sets display to none of the overlay
-  startButton.addEventListener('click', () => {
-    const overley = document.getElementById('overlay');
-    setTimeout(function() {
-      overlay.style.display = 'none'
-    }, 1000);
-    overlay.classList.add('fade');
-  });
+
 
   // function which takes the array and selects a random item of the array
 
   function getRandomPhraseAsArray(arr) {
     let i = Math.floor(Math.random() * phrases.length)
-    console.log(phrases[i]);
     return phrases[i].split("");
   };
 
@@ -39,22 +35,59 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < arr.length; i++) {
       const listItem = document.createElement('LI');
       listItem.textContent = arr[i];
-      if (listItem.textContent != " ") {
+      if (arr[i] != " ") {
         listItem.classList = "letter";
-      }
-      else {
+      } else {
         listItem.classList = "space";
       }
       phrase.appendChild(listItem)
     }
   }
 
-  const phraseArray = getRandomPhraseAsArray(phrases);
-  addPhraseToDisplay(phraseArray)
+  // Eventlistener which fades and sets display to none of the overlay
+
+  startButton.addEventListener('click', () => {
+    //setting missed always back to 0
+    missed = 0;
+    //fade the overlay away
+    const overley = document.getElementById('overlay');
+    setTimeout(function() {
+      overlay.style.display = 'none'
+    }, 1000);
+    overlay.classList.add('fade');
+    // clear the phrase list
+    let lettersInList = document.getElementsByClassName('letter');
+    let spacesInList = document.getElementsByClassName('space');
+
+    for (let i = lettersInList.length - 1; i >= 0; i--) {
+      lettersInList[i].parentNode.removeChild(lettersInList[i]);
+    }
+    for (let i = spacesInList.length - 1; i >= 0; i--) {
+      spacesInList[i].parentNode.removeChild(spacesInList[i]);
+    }
+
+    //generate and add  phrase
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray)
+
+    // clear the keyboard
+    let keys = document.querySelectorAll('BUTTON');
+
+    for (let i = 0; i < keys.length; i++) {
+      keys[i].removeAttribute('class');
+      keys[i].removeAttribute("disabled");
+    }
+
+    // set to full lifes again
+    for (let i = 0; i < 5; i++) {
+      lifes.firstElementChild.children[i].firstElementChild.src = "images/liveHeart.png";
+    }
+  });
 
 
 
 
+  // checks if the letter clicked matches any letter in the phrase
   function checkLetter(target) {
     const letter = document.querySelectorAll('.letter');
     const clickedLetter = target.textContent;
@@ -69,32 +102,26 @@ document.addEventListener('DOMContentLoaded', () => {
     return letterFound;
   }
 
-// THIS IS A ALTERNATIV SOLUTION WHICH I TRIED BUT IT DID NOT WORK
-// I WOULD LIKE TO KNOW WHY EXACTLY?
-  function checkLetterAlternativ(target) {
-    const letter = document.querySelectorAll('.letter');
-    const clickedLetter = target.textContent;
+// this function is used within the checkWin function
+  function winLose(className, resultText) {
+    overlay.classList = className;
+    overlay.style.display = "flex";
+    startButton.textContent = 'Reset';
+    title.innerHTML = resultText;
+  }
 
-    for (let i = 0; i < letter.length; i++) {
-      if (letter[i].textContent.toLocaleLowerCase() === clickedLetter) {
-        letter[i].classList.add("show");
-       const  letterFound = letter[i].textContent;
-       return letterFound;
-     } else {
-       return null;
-     }
+
+
+  function checkWin() {
+    const hasLetterClass = document.querySelectorAll('.letter');
+    const hasShowClass = document.querySelectorAll('.show');
+    if (hasLetterClass.length === hasShowClass.length) {
+      winLose('win', "You win yeaaaah")
+    }
+    if (missed >= 5) {
+      winLose('lose', "Oh dear maybe next time")
     }
   }
-  // THIS IS A ALTERNATIV SOLUTION WHICH I TRIED BUT IT DID NOT WORK
-  // I WOULD LIKE TO KNOW WHY EXACTLY?
-
-function removeLife () {
-  if (checkLetter(selectedButton) === null) {
-    lifes.firstElementChild.removeChild(tries[0]);
-    missed++;
-  }
-}
-
 
   // Eventlistener for each clicked Button
 
@@ -107,11 +134,13 @@ function removeLife () {
     checkLetter(selectedButton);
 
     // removing the lifes
-    if (checkLetter(selectedButton) === null) {
-      lifes.firstElementChild.removeChild(tries[0]);
+    if (checkLetter(selectedButton) === null && selectedButton.classList.contains('keyrow') != true) { // is this if statement needed or how can I fix the issue that it would take a life if clicked in the gaps
+
+      // lifes.firstElementChild.removeChild(tries[0] );
+      lifes.firstElementChild.children[missed].firstElementChild.src = "images/lostHeart.png"
       missed++;
     }
-
+    checkWin();
   });
 
   // Document loded even listener end
